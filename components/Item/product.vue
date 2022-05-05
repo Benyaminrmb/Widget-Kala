b5e271373fb867d30d66a51c072c6e44e661b300
benyaminrmb <benyaminrmb@gmail.com>
2022-05-05 07:53:47 +0430

    create product section

---
+++ components/Item/product.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="col-span-3 flex rounded-md flex-wrap border border-gray-200 p-3 transition duration-150 group transform hover:shadow-2xl"
+  >
+    <div class="w-full flex">
+      <img
+        src="~/assets/images/product1.png"
+        class="w-full rounded-lg"
+        alt=""
+      />
+    </div>
+    <div class="flex">
+      <h3 class="text-gray-800 font-light leading-6">
+        لنز سوپر واید گوشی موبایل F30 0.3X Super Wide JSJSJSJSJSJSJSJs
+      </h3>
+    </div>
+    <div class="flex text-orange-500 text-xs justify-end w-full">
+      <span>VIVU برند</span>
+    </div>
+    <div class="flex line-through text-gray-700 text-xl w-full">
+            <span
+              class="after:content-[attr(data-after)] after:text-sm items-center after:left-0 relative"
+              data-after="هزار تومان"
+            >150،000</span
+            >
+    </div>
+    <div
+      class="flex gap-x-4 items-center justify-between text-gray-700 text-xl w-full"
+    >
+            <span
+              class="after:content-[attr(data-after)] after:text-sm items-center after:left-0 relative"
+              data-after="هزار تومان"
+            >150،000</span
+            >
+
+      <div class="px-2 py-1 bg-gray-400 text-xs rounded">40%</div>
+      <div
+        class="border-2 group-hover:bg-gray-800 group-hover:text-white transition duration-150 items-center justify-center rounded-xl border-gray-800"
+      >
+        <button class="flex">
+          <span class="icon flex text-5xl icon-trolley"> </span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "product"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -17,13 +17,34 @@
       </div>
 
       <div class="grid mt-7 gap-x-7 grid-cols-12 justify-between">
-        <div class="col-span-10 flex">
-          <span class="flex">-</span>
-          <div class="flex text-xl">awdawd</div>
-          <div class="flex">zzz</div>
+        <div class="col-span-10 flex gap-x-5">
+          <span class="flex">
+            <span class="icon text-3xl icon-shield"></span>
+          </span>
+          <div class="flex text-gray-600 text-2xl">پرفروش ترین ها</div>
+          <div
+            class="relative after:w-0.5 after:absolute after:bg-gray-600 after:h-6 after:left-0 after:top-1"
+          ></div>
+
+          <ul class="tabs">
+            <li class="active">هدفون</li>
+            <li>هدفون</li>
+            <li>هدفون</li>
+            <li>هدفون</li>
+          </ul>
+        </div>
+        <div class="col-span-2 justify-end flex">
+          <button
+            class="border w-full rounded-md text-sm border-sky-500 text-sky-500 px-3"
+          >
+            همه محصولات را ببینید
+          </button>
         </div>
       </div>
 
+      <div class="grid mt-7 gap-x-7 grid-cols-12 justify-between">
+        <ItemProduct v-for="i in 4"></ItemProduct>
+      </div>
     </div>
   </div>
 </template>
